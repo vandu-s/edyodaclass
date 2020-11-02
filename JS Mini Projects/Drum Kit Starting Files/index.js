55946e9769630017c32063b93e1b6d5cf04addb5
vandana singandipe <vandana.singandipe@gmail.com>
2020-11-02 23:01:52 +0530

    new files added

---
+++ JS Mini Projects/Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+//var btnClick = document.querySelectorAll('button');
+document.querySelector("button").addEvenListener("click", function() {
+    alert('i got clicked');
+});
+
+//btnClick.onclick = handleClick;
